157dddace58fc05d45c1d18fb324b1aabc5dea7e
popote1 <yvan.bladet@gmail.com>
2024-10-16 22:51:37 +0200

    FirstCommit

---
+++ Assets/Scripte/PlayerController.cs
@@ -0,0 +1,91 @@
+
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private bool _IsPlaying;
+    
+    [SerializeField] private Transform _aimingPart;
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private LayerMask _AimingMask;
+    [SerializeField] private Transform _aimPoint;
+    [SerializeField] private RayEffect _prefabsRayEffect;
+    [SerializeField] private Image _imgReload;
+    [SerializeField] private float _reloadTime = 4;
+    [SerializeField] private GameObject _prefabsPSDeath;
+    
+
+    private CharacterController _cc;
+    private Camera _camera;
+    private float _timer;
+
+    private bool CanFire
+    {
+        get { return _timer >= _reloadTime; }
+    }
+    
+    void Start() {
+        _cc = GetComponent<CharacterController>();
+        _camera = Camera.main;
+    }
+
+    void Update() {
+        if (!_IsPlaying) return;
+        ManageMovement();
+        ManageAim();
+        ManagerFire();
+        ManageReload();
+    }
+
+    private void ManageReload() {
+        if (_timer > _reloadTime) return; 
+        _timer += Time.deltaTime;
+        _imgReload.fillAmount = _timer /_reloadTime;
+
+        if (_timer >= _reloadTime) {
+            _imgReload.enabled = false;
+            _timer = _reloadTime;
+        }
+    }
+
+    private void ManagerFire()
+    {
+        if (Input.GetButtonDown("Fire1")&& CanFire)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(_aimPoint.position, _aimPoint.forward, out hit)) {
+                RayEffect ray = Instantiate(_prefabsRayEffect, transform.position, quaternion.identity);
+                ray.SetUpEffect(_aimPoint.position, hit.point);
+                _timer = 0;
+                _imgReload.enabled = true;
+                _imgReload.fillAmount = 0;
+
+                if (hit.collider.transform.GetComponent<PlayerController>()) {
+                    hit.collider.transform.GetComponent<PlayerController>().Death();
+                }
+            }
+        }
+    }
+
+    private void ManageAim() {
+        RaycastHit hit;
+        if (Physics.Raycast(_camera.ScreenPointToRay(Input.mousePosition), out hit, _AimingMask)) {
+            Vector3 aimingVec = hit.point - _aimingPart.position;
+            aimingVec.y = 0;
+            _aimingPart.forward =aimingVec;
+        }
+    }
+
+    private void ManageMovement() {
+        Vector3 moveVec = new Vector3(-Input.GetAxisRaw("Horizontal"),0, -Input.GetAxisRaw("Vertical"));
+        _cc.Move(moveVec.normalized * (_moveSpeed*Time.deltaTime));
+    }
+
+    public void Death() {
+        Instantiate(_prefabsPSDeath, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripte/RayEffect.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class RayEffect : MonoBehaviour
+{
+    [SerializeField] private float _fadeTimer;
+    [SerializeField] private GameObject _PrefabsPS;
+    private LineRenderer _lineRenderer;
+    private Material _mat;
+    private float _timer;
+
+    public void SetUpEffect(Vector3 Start, Vector3 End) {
+        
+        _lineRenderer = GetComponent<LineRenderer>();
+        _mat = _lineRenderer.material;
+        _lineRenderer.positionCount = 3;
+        _lineRenderer.SetPositions(new []{Start, Vector3.Lerp(Start, End, 0.5f), End});
+
+        GameObject ps = Instantiate(_PrefabsPS, End, Quaternion.identity);
+        ps.transform.forward = Start - End;
+    }
+
+    private void Update() {
+        _timer += Time.deltaTime;
+        _lineRenderer.endWidth =Mathf.Lerp(1, 0f, _timer /_fadeTimer);
+        _lineRenderer.startWidth =Mathf.Lerp(1, 0f, _timer / _fadeTimer);
+        if (_timer >= _fadeTimer)
+        {
+            Destroy(gameObject);
+        }
+    } 
+}
